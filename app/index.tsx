884dc6ff96c8dd81f50e3f480606cbbf769d1b2e
Danny <76977155+DNYFZR@users.noreply.github.com>
2025-10-29 22:12:22 +0000

    updated image scaling

--- app/index.tsx
+++ app/index.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <View style={css.app}>
       <Text style={css.heading}>DELTA-IO</Text>
-      <View style={{ paddingTop: Dimensions.get("screen").height * 0.03 }}>
+      <View style={{ paddingTop: Dimensions.get("window").height * 0.03 }}>
         <Image
           style={{
-            width: Dimensions.get("screen").width * 0.4,
-            height: Dimensions.get("screen").height * 0.5,
+            width: Dimensions.get("window").width * 0.4,
+            height: Dimensions.get("window").height * 0.5,
             borderRadius: 16,
           }}
           source={require("@/assets/dial-io.jpg")}
